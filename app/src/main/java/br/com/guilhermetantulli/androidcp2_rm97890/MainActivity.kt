ea4bbca6960241e29bad474c1228b5eea30218a9
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-04-09 12:41:26 -0300

    Refactored: layout_height for all radios in activity_calculator.xml

--- app/src/main/java/br/com/guilhermetantulli/androidcp2_rm97890/MainActivity.kt
+++ app/src/main/java/br/com/guilhermetantulli/androidcp2_rm97890/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     // Função para exibir a assinatura do aplicativo
     private fun showSignature() {
         val mySignature = """
